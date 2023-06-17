f8b20f703261c2680e680cfc2a738c309182c5bb
mbul03 <marsyaverocp03@gmail.com>
2023-06-17 13:31:22 +0700

    first commit
    First Commit

---
+++ Perun's Space/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void playGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void BackToMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("How to Play");
+    }
+}
